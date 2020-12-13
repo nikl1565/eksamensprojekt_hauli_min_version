d468e4cb3e0b3def2e31f2635e55ea541025f9b7
Eva Kjær Hansen <eva@Evas-MacBook-Pro.local>
2020-12-13 18:53:19 +0100

    header done???? :DDDDD

--- JS/global.js
+++ JS/global.js
@@ -7,11 +7,11 @@ function init() {
 
 function toggleHeaderBurgerIcon() {
     console.log("toggle menu");
-    document.querySelector(".css_header_mobile").classList.toggle("css_header_mobile_open");
+    document.querySelector("header").classList.toggle("css_header_mobile_open");
 
     document.querySelector(".kurv").classList.toggle("kurv_kom_frem");
 
-    let isOpen = document.querySelector(".css_header_mobile").classList.contains("css_header_mobile_open");
+    let isOpen = document.querySelector("header").classList.contains("css_header_mobile_open");
 
     if (isOpen == true) {
         document.querySelector(".js_header_burger_icon").innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
